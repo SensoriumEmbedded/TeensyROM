f78b8ccdb1e68b04e695bca89ea8840d8ac3968c
Travis Smith <travis@sensoriumembedded.com>
2023-01-16 16:09:13 -0800

    Emulates H/L/16k ROMs

--- TeensyROM.h
+++ TeensyROM.h
@@ -1,48 +1,73 @@
 
-const uint8_t AddrBusPins[] = {19,18,14,15,40,41,17,16,22,23,20,21,38,39,26,27};
 
-#define DataCEn_PIN        35     
-#define IO1n_PIN           24  
 #define PHI2_PIN            1  
-#define R_Wn_PIN            0  
+const uint8_t InputPins[] = {
+   19,18,14,15,40,41,17,16,22,23,20,21,38,39,26,27,  //address bus
+   2, 3, 4, 5, PHI2_PIN, 0,         // IO1n, IO2n, ROML, ROMH, PHI2_PIN, R_Wn
+   25,     //BA
+   };
+
+const uint8_t OutputPins[] = {
+   35, 9, 32, 6, // DataCEn, ExROM, Game, Reset
+   34,33,    //debug
+   };
 
 #define ReadGPIO6          (*(volatile uint32_t *)IMXRT_GPIO6_ADDRESS)
-#define GP_R_Wn(r)         (r & CORE_PIN0_BITMASK)
-#define GP_IO1n(r)         (r & CORE_PIN24_BITMASK)
-#define GP_Address(r)      ((r >> CORE_PIN19_BIT) & 0xFFFF)  // bits 16-31 contain address bus, in order 
-                           
+#define GP6_R_Wn(r)        (r & CORE_PIN0_BITMASK)
+#define GP6_Address(r)     ((r >> CORE_PIN19_BIT) & 0xFFFF)  // bits 16-31 contain address bus, in order 
+
+#define ReadGPIO9          (*(volatile uint32_t *)IMXRT_GPIO9_ADDRESS)
+#define GP9_IO1n(r)        (r & CORE_PIN2_BITMASK)
+#define GP9_IO2n(r)        (r & CORE_PIN3_BITMASK)
+#define GP9_ROML(r)        (r & CORE_PIN4_BITMASK)
+#define GP9_ROMH(r)        (r & CORE_PIN5_BITMASK)
+#define GP9_BA(r)          (r & CORE_PIN29_BITMASK)
+
 #define ReadGPIO7          (*(volatile uint32_t *)IMXRT_GPIO7_ADDRESS)
 #define GP7_DataMask       0xF000F   //CORE_PIN10,12,11,13,8,7,36,37_BITMASK
+#define SetDataPortDirOut  CORE_PIN10_DDRREG |= GP7_DataMask
+#define SetDataPortDirIn   CORE_PIN10_DDRREG &= ~GP7_DataMask
+
 #define DataBufDisable     CORE_PIN35_PORTSET = CORE_PIN35_BITMASK
 #define DataBufEnable      CORE_PIN35_PORTCLEAR = CORE_PIN35_BITMASK
-#define SetDataPortWrite   CORE_PIN10_DDRREG |= GP7_DataMask
-#define SetDataPortRead    CORE_PIN10_DDRREG &= ~GP7_DataMask
+#define SetResetAssert     CORE_PIN6_PORTSET = CORE_PIN6_BITMASK
+#define SetResetDeassert   CORE_PIN6_PORTCLEAR = CORE_PIN6_BITMASK 
+#define SetExROMAssert     CORE_PIN9_PORTSET = CORE_PIN9_BITMASK
+#define SetExROMDeassert   CORE_PIN9_PORTCLEAR = CORE_PIN9_BITMASK 
+#define SetGameAssert      CORE_PIN32_PORTSET = CORE_PIN32_BITMASK
+#define SetGameDeassert    CORE_PIN32_PORTCLEAR = CORE_PIN32_BITMASK 
+#define SetDebugAssert     CORE_PIN34_PORTSET = CORE_PIN34_BITMASK
+#define SetDebugDeassert   CORE_PIN34_PORTCLEAR = CORE_PIN34_BITMASK 
+#define SetDebug2Assert    CORE_PIN33_PORTSET = CORE_PIN33_BITMASK
+#define SetDebug2Deassert  CORE_PIN33_PORTCLEAR = CORE_PIN33_BITMASK 
 
-#define Wait_nS(S, N)      while(ARM_DWT_CYCCNT - S < ((F_CPU_ACTUAL>>16) * N) / (1000000000UL>>16)) 
+#define RESET_CYCLECOUNT   { ARM_DEMCR |= ARM_DEMCR_TRCENA; ARM_DWT_CTRL |= ARM_DWT_CTRL_CYCCNTENA; ARM_DWT_CYCCNT = 0; }
+#define WaitUntil_nS(N)      while(ARM_DWT_CYCCNT < N * 0.816) 
+   //was ARM_DWT_CYCCNT < ((F_CPU_ACTUAL>>16) * N) / (1000000000UL>>16)     F_CPU_ACTUAL=816000000  /1000000000 = 0.816
 // all times starting from Phi2 rising (interrupt). 
-//could move to interrupts on IO1/2/ROML/H, or decode full bus instead.
-#define nS_PLAprop         100 //delay through PLA to decode address (IO1/2, ROML/H), have measured >100nS from Phi2 to IO1 (delayed through PLA, etc)
+#define nS_RWnReady        20  // Phi2 rise to RWn valid
+#define nS_PLAprop         100 //100 //delay through PLA to decode address (IO1/2, ROML/H), have measured >100nS from Phi2 to IO1 (delayed through PLA, etc)
 #define nS_DataSetup       325 //On a write, when to latch data bus. spec calls for 150-200nS min to Data valid for write opperation (TMDS)
-#define nS_DataHold        375  //On a read, when to stop driving the data bus, spec calls for >430
+#define nS_DataHold        375 //375  //On a read, when to stop driving the data bus, spec calls for >430
 
-__attribute__((always_inline)) inline void DataPortWriteWait(uint8_t Data, uint32_t StartCycles)
+__attribute__((always_inline)) inline void DataPortWriteWait(uint8_t Data)
 {
    DataBufEnable; 
    register uint32_t RegBits = (Data & 0x0F) | ((Data & 0xF0) << 12);
    CORE_PIN7_PORTSET = RegBits;
    CORE_PIN7_PORTCLEAR = ~RegBits & GP7_DataMask;
-   Wait_nS(StartCycles, nS_DataHold);  //could poll Phi2 for falling edge...  only 30nS typ hold time
+   WaitUntil_nS(nS_DataHold);  //could poll Phi2 for falling edge...  only 30nS typ hold time
    DataBufDisable;
 }
 
-__attribute__(( always_inline )) inline uint8_t DataPortWaitRead(uint32_t StartCycles)
+__attribute__(( always_inline )) inline uint8_t DataPortWaitRead()
 {
-   SetDataPortRead; //set data ports to inputs         //data port set to read previously
+   SetDataPortDirIn; //set data ports to inputs         //data port set to read previously
    DataBufEnable; //enable external buffer
-   Wait_nS(StartCycles, nS_DataSetup);  //could poll Phi2 for falling edge...  only 30nS typ hold time
+   WaitUntil_nS(nS_DataSetup);  //could poll Phi2 for falling edge...  only 30nS typ hold time
    register uint32_t DataIn = ReadGPIO7;
    DataBufDisable;
-   SetDataPortWrite; //set data ports to outputs (default)
+   SetDataPortDirOut; //set data ports to outputs (default)
    return ((DataIn & 0x0F) | ((DataIn >> 12) & 0xF0));
 }
 
--- TeensyROM.ino
+++ TeensyROM.ino
@@ -2,8 +2,15 @@
 //Compile with 816MHz (overclock) option set
 
 #include "TeensyROM.h"
+#include "ROMs\586220ast_Diagnostics.h"
+#include "ROMs\Jupiter_Lander.h"
+#include "ROMs\781220_Dead_Test.h"
+#include "ROMs\Donkey_Kong.BIN.h"
 
 uint8_t IO1_RAM[256];
+volatile uint8_t doReset = true;
+static const unsigned char *HIROM_Image = NULL;
+static const unsigned char *LOROM_Image = NULL;
 
 void setup() 
 {
@@ -11,62 +18,123 @@ void setup()
    Serial.println(F("File: " __FILE__ ));
    Serial.println(F("Date: " __DATE__ ));
    Serial.println(F("Time: " __TIME__ ));
-
-   pinMode(DataCEn_PIN, OUTPUT);  
-   DataBufDisable; //buffer disabled
-   for(uint8_t PinNum=0; PinNum<sizeof(AddrBusPins); PinNum++) pinMode(AddrBusPins[PinNum], INPUT); 
-   SetDataPortWrite; //default to output (for C64 Read)
+   Serial.print(F_CPU_ACTUAL);
+   Serial.println(" Hz");
    
-   pinMode(IO1n_PIN, INPUT);  
-   pinMode(PHI2_PIN, INPUT);  
-   pinMode(R_Wn_PIN, INPUT);  
-   //pinMode(IN2_PIN, INPUT_PULLUP);  
- 
-   attachInterrupt( digitalPinToInterrupt( PHI2_PIN ), isrPHI2, RISING );
+   SetResetDeassert;
+   DataBufDisable; //buffer disabled
+   for(uint8_t PinNum=0; PinNum<sizeof(OutputPins); PinNum++) pinMode(OutputPins[PinNum], OUTPUT); 
+   SetDataPortDirOut; //default to output (for C64 Read)
+   SetExROMDeassert;
+   SetGameDeassert; 
 
+   for(uint8_t PinNum=0; PinNum<sizeof(InputPins); PinNum++) pinMode(InputPins[PinNum], INPUT); 
+   
+   attachInterrupt( digitalPinToInterrupt( PHI2_PIN ), isrPHI2, RISING );
+   
+   SetDebugDeassert;
+   SetDebug2Deassert;
+   //SetExROMAssert;
+   //SetGameAssert;
+  
    Serial.print("TeensyROM is on-line\n");
+
 } 
   
 void loop()
 {
-
+   if (doReset)
+   {
+      doReset=false;
+      Serial.println("Resetting C64"); 
+      SetResetAssert; 
+      delay(5); 
+      SetResetDeassert;
+   }
 }
 
 FASTRUN void isrPHI2()
 {
-   register uint32_t StartCycles = ARM_DWT_CYCCNT;  //mark isr start "time"
-   register uint32_t GPIO_6 = ReadGPIO6; //Address bus and R/*W are valid on Phi2 rising, Read now:
+   RESET_CYCLECOUNT;
    
-   //if (!GP_R_Wn(GPIO_6)) //low (Write)
-   //{
-   //   //SetDataPortRead; //set data ports to inputs
-   //   //DataBufEnable; //enable external buffer
-   //}
+ 	SetDebug2Assert;
+   //SetDebugDeassert;
+   WaitUntil_nS(nS_RWnReady); 
+
+   register uint32_t GPIO_6 = ReadGPIO6; //Address bus and (almost) R/*W are valid on Phi2 rising, Read now
+    
+   register uint16_t Address = GP6_Address(GPIO_6); //parse out address
+   register bool IsRead = GP6_R_Wn(GPIO_6);
    
-   register uint16_t Address = GP_Address(GPIO_6); //parse out address
+ 	WaitUntil_nS(nS_PLAprop); 
    
- 	Wait_nS(StartCycles, nS_PLAprop); 
-   
-   GPIO_6 = ReadGPIO6; //read it again
-   if (!GP_IO1n(GPIO_6)) //DExx address space
+   register uint32_t GPIO_9 = ReadGPIO9; //Now read the derived signals
+   if (!GP9_IO1n(GPIO_9)) //IO1: DExx address space
    {
-      if (GP_R_Wn(GPIO_6)) //High (Read)
+      if (IsRead) //High (Read)
       {
-         DataPortWriteWait(IO1_RAM[Address & 0xFF], StartCycles);  
+         DataPortWriteWait(IO1_RAM[Address & 0xFF]);  
          //Serial.printf("Rd %d from %d\n", IO1_RAM[Address & 0xFF], Address);
       }
       else  //write
       {
-         IO1_RAM[Address & 0xFF] = DataPortWaitRead(StartCycles); 
+         IO1_RAM[Address & 0xFF] = DataPortWaitRead(); 
          Serial.printf("Wr %d to %04x\n", IO1_RAM[Address & 0xFF], Address);
+         if (Address == 57000)   //DEA8
+         {
+            switch (IO1_RAM[Address & 0xFF]) 
+            {
+            case 1:
+               //reset C64
+               doReset=true;
+               break;
+            case 2:
+               Serial.println("Starting ExROM/ROML cartridge");
+               SetGameDeassert;
+               SetExROMAssert;
+               LOROM_Image = a586220ast_Diagnostics_BIN;
+               HIROM_Image = NULL;
+               doReset=true;
+               //works with H/L/3FFF on both
+               //only requires L/1FFF
+               break;
+            case 3:
+               Serial.println("Starting GAME/ROMH cartridge");
+               SetExROMDeassert;
+               SetGameAssert;
+               LOROM_Image = NULL;
+               HIROM_Image = a781220_Dead_Test_BIN;
+               //ROM_Image = Jupiter_Lander_BIN;
+               doReset=true;
+               //requires ROMH and 1FFF as only response
+               break;
+            case 4:
+               Serial.println("Starting GAME/ROMH/ExROM/ROML (16k) cartridge");
+               SetExROMAssert;
+               SetGameAssert;
+               LOROM_Image = Donkey_Kong_BIN;
+               HIROM_Image = Donkey_Kong_BIN + 0x2000;
+               doReset=true;
+               //works with H/L/3FFF on both
+               //only requires H(1FFF | 2000)/L(1FFF)
+               break;
+            }
+
+         }
       }
-      
-   }
-   //DataBufDisable;
-   //SetDataPortWrite; //(default, not needed) set data ports to outputs (default)
+   }  //IO1
+   else if (!GP9_ROML(GPIO_9)) //ROML: 8000-9FFF address space, read only
+   {
+      if (LOROM_Image!=NULL) DataPortWriteWait(LOROM_Image[Address & 0x1FFF]);  
+   }  //ROML
+   else if (!GP9_ROMH(GPIO_9)) //ROMH: A000-BFFF or E000-FFFF address space, read only
+   {
+      if (HIROM_Image!=NULL) DataPortWriteWait(HIROM_Image[(Address & 0x1FFF)]); 
+   }  //ROMH
    
+ 
    //now the time-sensitive work is done, have a few hundred nS until the next interrupt...
-   
+   SetDebug2Deassert;
 }
 
 
