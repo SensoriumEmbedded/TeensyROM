7d9faa3852aa937a929f01dfe6c9eedec5c830ae
Travis Smith <travis@sensoriumembedded.com>
2023-11-21 14:21:40 -0800

    File x-fer using  PostFileCommand() instead of ReceiveFile() ReceiveFile() removed

--- Source/Teensy/SerUSBIO.ino
+++ Source/Teensy/SerUSBIO.ino
@@ -55,14 +55,12 @@ FLASHMEM void ServiceSerial()
                Serial.printf("TeensyROM %s ready!\n", strVersionNumber);
                break;
             case 0xAA: //file x-fer pc->TR
-               ReceiveFile();        
-               break;
             case 0xBB:  // v2 file x-fer pc->TR.  For use with v2 UI.
-                PostFileCommand();
-                break;
+               PostFileCommand();
+               break;
             case 0xDD:  // v2 directory listing from TR
-                GetDirectoryCommand();
-                break;
+               GetDirectoryCommand();
+               break;
             case 0x44: //Launch File
                if(LaunchFile()) Serial.println("Launched!");  
                else Serial.println("Launch Failed");  
@@ -413,90 +411,6 @@ FLASHMEM bool ReceiveFileName(uint32_t *SD_nUSB, char *FileNamePath)
          return false;
       }
    }
-}
-               
-FLASHMEM void ReceiveFile()
-{ 
-   //   App: SendFileToken 0x64AA
-   //Teensy: AckToken 0x64CC
-   //   App: Send Length(4), CS(2), SD_nUSB(1), 
-   //          DestPath/Name(up to MaxNamePathLength, null term)
-   //Teensy: AckToken 0x64CC
-   //   App: Send file(length)
-   //Teensy: AckToken 0x64CC on Pass,  0x9b7f on Fail
-   
-
-   //send file token has been received, only 2 byte responses until after final response
-   SendU16(AckToken);
-   
-   uint32_t len;
-   if (!GetUInt(&len, 4)) return;
-   
-   uint32_t CheckSum;
-   if (!GetUInt(&CheckSum, 2)) return;
-   
-   uint32_t SD_nUSB;
-   char FileNamePath[MaxNamePathLength];
-   if (!ReceiveFileName(&SD_nUSB, FileNamePath)) return;
-   
-   FS *sourceFS = &firstPartition;
-   if (SD_nUSB)
-   {
-      if (!SD.begin(BUILTIN_SDCARD))
-      {
-         //SendU16(FailToken); //app will timeout waiting for SD init
-         Serial.printf("No SD card?\n");  
-         return;               
-      }
-      sourceFS = &SD;   
-   }
-   
-   if (sourceFS->exists(FileNamePath))
-   {
-      SendU16(FailToken);
-      Serial.printf("File already exists.\n");  
-      return;      
-   }
-   
-   File myFile = sourceFS->open(FileNamePath, FILE_WRITE);
-   if (!myFile) 
-   {
-      SendU16(FailToken);
-      Serial.printf("Could not open for write: %s:%s\n", (SD_nUSB ? "SD" : "USB"), FileNamePath);  
-      return;
-   }
-   
-   SendU16(AckToken); //starts file data streaming 
-   //Serial.printf("Len: %lu  CS: 0x%04x\n %s:%s\n", len, CheckSum, (SD_nUSB ? "SD" : "USB"), FileNamePath);
-  
-   uint32_t bytenum = 0;
-   uint8_t ByteIn;
-   while(bytenum < len)
-   {
-      if(!SerialAvailabeTimeout())
-      {
-         SendU16(FailToken);
-         Serial.printf("Rec %lu of %lu bytes\n", bytenum, len);
-         myFile.close();
-         return;
-      }
-      //uint8_t ByteIn = Serial.read();
-      myFile.write(ByteIn = Serial.read());
-      CheckSum-=ByteIn;
-      bytenum++;
-   }  
-   
-   myFile.close();
-   
-   CheckSum &= 0xffff;
-   if (CheckSum!=0)
-   {  //Failed
-      SendU16(FailToken);
-      Serial.printf("CS Failed! RCS:%lu\n", CheckSum);
-      return;
-   }   
-   
-   SendU16(AckToken); //success!
 }
 
 FLASHMEM bool GetUInt(uint32_t *InVal, uint8_t NumBytes)
