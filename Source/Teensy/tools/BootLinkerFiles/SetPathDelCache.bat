2817a6252f12943d6bb92ff288bc5131bd08ba92
SensoriumEmbedded <travis@sensoriumembedded.com>
2025-10-16 12:51:50 -0700

    Teensyduino version path build variable update

--- Source/Teensy/tools/BootLinkerFiles/SetPathDelCache.bat
+++ Source/Teensy/tools/BootLinkerFiles/SetPathDelCache.bat
@@ -1,6 +1,7 @@
 
 set LocalPath=C:\Users\trav\AppData\Local
-set LocalT4CorePath=%LocalPath%\Arduino15\packages\teensy\hardware\avr\0.60.4\cores\teensy4
+set LocalT4CorePath=%LocalPath%\Arduino15\packages\teensy\hardware\avr\1.59.0\cores\teensy4
+::set LocalT4CorePath=%LocalPath%\Arduino15\packages\teensy\hardware\avr\0.60.4\cores\teensy4
 
 :: delete temp local cache:
 rmdir /s /q "%LocalPath%\Temp\arduino\cores"
