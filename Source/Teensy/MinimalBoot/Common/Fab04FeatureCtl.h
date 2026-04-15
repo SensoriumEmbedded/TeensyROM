f87c18682860b239dfbf3dd1106219f786da1e1b
Travis Smith <travis@sensoriumembedded.com>
2026-04-15 14:59:03 -0700

    Warning about defining Fab04_Features

--- Source/Teensy/MinimalBoot/Common/Fab04FeatureCtl.h
+++ Source/Teensy/MinimalBoot/Common/Fab04FeatureCtl.h
@@ -1,3 +1,7 @@
 
-//comment out for fab 0.2x or 0.3 build.  Uncomment for fab 0.4 PCB only!
+//comment out for fab 0.2x or 0.3 build.  Uncomment for fab 0.4 PCB manual build only
+
+// Note: defining Fab04_Features here will *over-ride* the build scripts and build only for TR+
+//  Only un-comment if you know what you're doing!
+
  // #define Fab04_Features    //enables all Fab 0.4 build options
