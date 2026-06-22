86dbf548336781cdae44e1d0e6d7398e3631e9d2
SensoriumEmbedded <travis@sensoriumembedded.com>
2026-06-22 14:37:42 -0700

    Help screen display as separate program

---
+++ Source/C64/TRHelpScreens/source/Pg_HelpInfo2.asm
@@ -0,0 +1,49 @@
+; MIT License
+; 
+; Copyright (c) 2026 Travis Smith
+; 
+; Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+; and associated documentation files (the "Software"), to deal in the Software without 
+; restriction, including without limitation the rights to use, copy, modify, merge, publish, 
+; distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom 
+; the Software is furnished to do so, subject to the following conditions:
+; 
+; The above copyright notice and this permission notice shall be included in all copies or 
+; substantial portions of the Software.
+; 
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+; BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+; DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+HelpMenu2:
+   jsr CommonInit ;print banner and common keys/page#
+
+   lda #<MsgHelpMenu2
+   ldy #>MsgHelpMenu2
+   jsr PrintString 
+   
+jmp WaitHelpMenuKey ;same as other help screen, just waiting for common keys  
+   
+MsgHelpMenu2:
+   !tx EscC,EscSourcesColor, ChrRvsOn, " Directory Alt File Select ", ChrReturn, ChrReturn
+
+   !tx EscC,EscNameColor, " Highlighted File Select Options:", ChrReturn
+   !tx                      EscC,EscOptionColor, " <Ret>",                                EscC,EscSourcesColor, "  Launch file/enter dir", ChrReturn
+   !tx                      EscC,EscOptionColor, " !\"#$%",                                EscC,EscSourcesColor, "  Set as Hot Key 1-5", ChrReturn
+   !tx EscC,EscArgSpaces+5, EscC,EscOptionColor, "A",                                      EscC,EscSourcesColor, "  Set as Auto-Launch", ChrReturn
+   !tx EscC,EscArgSpaces+5, EscC,EscOptionColor, ChrLeftArrow,    EscC,EscNameColor, " *", EscC,EscSourcesColor, "Write NFC tag: Highlighted File", ChrReturn
+   !tx EscC,EscArgSpaces+5, EscC,EscOptionColor, ChrQuestionMark, EscC,EscNameColor, " *", EscC,EscSourcesColor, "Write NFC tag: Random in Dir", ChrReturn
+   !tx EscC,EscArgSpaces+5, EscC,EscOptionColor, "M",             EscC,EscNameColor, " *", EscC,EscSourcesColor, "Meatloaf: Mount/Launch Dxx", ChrReturn
+   !tx EscC,EscArgSpaces+5, EscC,EscOptionColor, "K",             EscC,EscNameColor, " +", EscC,EscSourcesColor, "Set as Kernal Replace File", ChrReturn
+   !tx EscC,EscArgSpaces+5, EscC,EscOptionColor, "R",             EscC,EscNameColor, " +", EscC,EscSourcesColor, "Set as REU Pre-load/save File", ChrReturn
+   !tx ChrReturn
+   !tx EscC,EscNameColor, "   *:", EscC,EscSourcesColor, " External HW Required", ChrReturn
+   !tx EscC,EscNameColor, "   +:", EscC,EscSourcesColor, " TeensyROM+ Only", ChrReturn
+   !tx ChrReturn
+   !tx EscC,EscNameColor, " For additional help, see:", ChrReturn
+   !tx EscC,EscSourcesColor, " github.com/SensoriumEmbedded/TeensyROM"
+   !tx 0
---
+++ Source/C64/TRHelpScreens/source/TRHelpScreens.asm
@@ -0,0 +1,66 @@
+
+   !convtab pet   ;key in and text out conv to PetSCII throughout
+   !src "../MainMenuCRT/source/c64defs.i"  ;C64 colors, mem locations, etc.
+   !src "../MainMenuCRT/source/Menu_Regs.i"  ;IO space registers matching Teensy code
+
+   ;!set DbgOffline = 1   ;if defined, skips all waits/dependancies on TR HW
+
+
+   * = $0801    ;BasicStart
+
+   !word BasicEnd                  ; BASIC pointer to next line
+   !word 2026                      ; BASIC line number (setting to year)
+   !byte $9e                       ; BASIC token code for "SYS" command
+   !byte (SysAddress/1000)+$30     ; Calculate 1st digit of SysAddress
+   !byte ((SysAddress/100)%10)+$30 ; Calculate 2nd digit of SysAddress
+   !byte ((SysAddress/10)%10)+$30  ; Calculate 3rd digit of SysAddress
+   !byte SysAddress%10+$30         ; Calculate 4th digit of SysAddress
+   !byte $00                       ; BASIC end of line marker
+BasicEnd    
+   !word $0000                     ; BASIC end of program
+
+   !src "source/SupportFunctions.asm"
+   !src "source/StringFunctions.asm"
+
+SysAddress:
+
+;screen setup:     
+!ifndef DbgOffline {
+   ;copy colors from IO1 to local RAM
+   ldx #NumColorRefs
+-  lda rwRegColorRefStart-1+IO1Port, x ;zero based offset
+   sta TblEscC-1, x
+   dex
+   bne -
+}  
+   jsr ScreenColorOnly ;update screen colors now that we have them
+
+   ;store default register for 12/24 hour time display locally
+   lda rwRegPwrUpDefaults+IO1Port
+   and #rpudClock12_24hr
+   sta smc24HourClockDisp+1  ;local copy of 12/24 hr
+   
+   ;always read current TOD from Teensy RTC, battery backed or not...
+   jsr SetC64TODfromRTC
+
+   jmp PageUpdate  ;jump to default page
+
+bPageNum:  ;current page num/default
+   !byte 0
+   
+bTotalPages: ;num of pages in tblSettingsPages
+   !byte 2
+   
+tblSettingsPages:
+   ;!word GeneralSettings
+   !word HelpMenu
+   !word HelpMenu2
+      
+   ;settings pages:
+   !src "source/Pg_HelpInfo.asm"
+   !src "source/Pg_HelpInfo2.asm"
+   
+EndOfCode:
+   !byte $00 ;byte to mark end address in build report
+
+
