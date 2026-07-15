680ece32f0b8daa6f2bbbe905a2209c1c4648e4c
Travis Smith <travis@sensoriumembedded.com>
2026-07-15 16:44:43 -0700

    TR External port test- assy program

---
+++ Source/C64/TRExtPortCheck/source/SupportFunctions.asm
@@ -0,0 +1,78 @@
+
+ScreenColorOnly:
+   lda TblEscC+EscBorderColor
+   sta BorderColorReg
+   lda TblEscC+EscBackgndColor
+   sta BackgndColorReg
+   rts
+
+SetRTCfromEthernet:
+   ; Synchs the Teensy RTC with time acquired from Ethernet
+   lda #rCtlSetRTCfromNetWAIT
+   sta wRegControl+IO1Port
+   jsr WaitForTRDots ;use WaitForTRWaitMsg instead?
+   rts
+ 
+SetC64TODfromRTC:
+   ;Sets the C64 TOD clock from the Teensy RTC (with timezone offset)
+   lda #rCtlC64TODfromRTCWAIT
+   sta wRegControl+IO1Port
+   jsr WaitForTRDots 
+SetC64TODfromRTC_Preloaded:
+   lda rRegLastHourBCD+IO1Port
+   sta TODHoursBCD  ;stop TOD regs incrementing
+   lda rRegLastMinBCD+IO1Port
+   sta TODMinBCD
+   lda rRegLastSecBCD+IO1Port
+   sta TODSecBCD
+   lda #9
+   sta TODTenthSecBCD ;have to write 10ths to release latch, start incrementing
+   rts
+
+;WaitForTR* uses acc, X and Y
+WaitForTRDots:  ;prints a dot per second while waiting, doesn't move cursor
+   ldy TODSecBCD ;reset dot second counter
+   jmp WaitForTRMain
+WaitForTRWaitMsg:  ;Print Waiting message in upper right and waits
+   ldx #1 ;row   Show "Waiting:" over time disp
+   ldy #29  ;col
+   clc
+   jsr SetCursor
+   lda #<MsgWaiting
+   ldy #>MsgWaiting
+   jsr PrintString
+   ldy #$ff ;don't print dots
+WaitForTRMain   ;Main wait loop
+!ifndef DbgOffline {
+   inc C64ScreenRAM+40*2-2 ;spinner @ top-1, right-1
+   cpy #$ff
+   beq +    ;skip dot printing if wait message selected
+   cpy TODSecBCD  ;no latch/unlatch needed for only reading seconds
+   beq +
+   ldy TODSecBCD  ;print 1 dot/sec while waiting
+   lda #'.'
+   jsr SendChar
++  lda rwRegStatus+IO1Port
+   cmp #rsC64Message
+   beq ++
+   cmp #rsReady
+   bne WaitForTRMain
+++ ldx#5 ;require 1+5 consecutive reads of same rsC64Message/rsReady to continue
+-  cmp rwRegStatus+IO1Port
+   bne WaitForTRMain
+   dex
+   bne -
+   ; the ball is in the C64 court, either done or display message and cont waiting
+   cmp #rsReady
+   beq +
+   ; display message:
+   lda #rsstSerialStringBuf ;pre-populated message
+   jsr PrintSerialString
+   lda #rsContinue    ;tell fw we're done reading msg, continue
+   sta rwRegStatus+IO1Port
+   jmp WaitForTRMain
+}
++  rts
+
+MsgWaiting:
+   !tx EscC,EscTimeColor, " Waiting:", 0
---
+++ Source/C64/TRExtPortCheck/source/TRExtPortCheck.asm
@@ -0,0 +1,114 @@
+
+   !convtab pet   ;key in and text out conv to PetSCII throughout
+   !src "../MainMenuCRT/source/c64defs.i"  ;C64 colors, mem locations, etc.
+   !src "../MainMenuCRT/source/Menu_Regs.i"  ;IO space registers matching Teensy code
+
+   ;!set DbgOffline = 1   ;if defined, skips all waits/dependancies on TR HW
+
+
+   * = $0801    ;BasicStart
+
+   !word BasicEnd                  ; BASIC pointer to next line
+   !word 2026                      ; BASIC line number (setting to year)
+   !byte $9e                       ; BASIC token code for "SYS" command
+   !byte (SysAddress/1000)+$30     ; Calculate 1st digit of SysAddress
+   !byte ((SysAddress/100)%10)+$30 ; Calculate 2nd digit of SysAddress
+   !byte ((SysAddress/10)%10)+$30  ; Calculate 3rd digit of SysAddress
+   !byte SysAddress%10+$30         ; Calculate 4th digit of SysAddress
+   !byte $00                       ; BASIC end of line marker
+BasicEnd    
+   !word $0000                     ; BASIC end of program
+
+   !src "source/SupportFunctions.asm"
+   !src "source/StringFunctions.asm"
+
+SysAddress:
+
+;screen setup:     
+!ifndef DbgOffline {
+   ;copy colors from IO1 to local RAM
+   ldx #NumColorRefs
+-  lda rwRegColorRefStart-1+IO1Port, x ;zero based offset
+   sta TblEscC-1, x
+   dex
+   bne -
+}  
+   jsr ScreenColorOnly ;update screen colors now that we have them
+
+   ;store default register for 12/24 hour time display locally
+   lda rwRegPwrUpDefaults+IO1Port
+   and #rpudClock12_24hr
+   sta smc24HourClockDisp+1  ;local copy of 12/24 hr
+   
+   jsr PrintBanner
+   lda #<MsgExtPortCheck
+   ldy #>MsgExtPortCheck
+   jsr PrintString 
+   
+; -----------  Start of test  -----------
+
+  
+   ;jsr DisplayTime ;do it once here
+   jsr SetRTCfromEthernet  ; establishes net connection early
+   jsr SetC64TODfromRTC   ;do this even if not part of test
+
+   lda #rCtlExtPortCheckWAIT
+   sta wRegControl+IO1Port
+   jsr WaitForTRDots   
+
+   lda rwRegScratch+IO1Port  ; 0=fail, 1=pass
+   beq +
+   lda #<MsgPassed
+   ldy #>MsgPassed
+   jmp ++
++  lda #<MsgFailed
+   ldy #>MsgFailed
+++ jsr PrintString 
+   
+-  jsr GetIn    
+   bne -    ;clear the kbd queue in case of accidental press
+
+   lda #<MsgAnyKey  ;wait for any key to continue 
+   ldy #>MsgAnyKey
+   jsr PrintString 
+-  jsr DisplayTime  
+   jsr GetIn   
+   beq -
+
+   lda #rCtlReturnToMainMenu 
+   sta wRegControl+IO1Port
+   ;C64 will be reset...
+
+MsgExtPortCheck:
+   !tx ChrReturn, EscC,EscSourcesColor, ChrRvsOn, " TeensyROM External Port Checks ", ChrReturn
+   ;expected interfaces:
+   ;Active Ethernet connection
+   ;SD card with at least 1 file
+   ;USB Thumb Drive with at least 1 file
+   ;Host computer to monitor USB device port(?)
+   !tx EscC, EscNameColor
+   !tx 0 
+   
+MsgPassed:
+   !tx ChrReturn, ChrReturn, EscC,EscSourcesColor, ChrRvsOn, " External Port Check Passed.", ChrReturn, ChrReturn
+   !tx EscC,EscOptionColor, "  Verify the following:", ChrReturn
+   !tx "   * Ethernet Port Green LED is on", ChrReturn
+   !tx "   * TR main LED (by Menu button) is on", ChrReturn
+   !tx "   * Serial output \"Hello from TR at..\"", ChrReturn
+   !tx "   * Check time shown above.", ChrReturn
+   !tx "   * RTC batt: cycle power & recheck", ChrReturn
+   !tx 0 
+
+MsgFailed:
+   !tx ChrReturn, EscC,EscOptionColor, ChrRvsOn, " External Port Check Failed!", ChrReturn
+   !tx 0 
+
+MsgAnyKey:
+   !tx ChrReturn, EscC,EscOptionColor, "Press any key to return", ChrReturn
+   !tx 0
+
+   
+EndOfCode:
+   !byte $00 ;byte to mark end address in build report
+
+
