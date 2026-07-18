ead15fc35b0798643e00902e469de74e73dc3c4a
Travis Smith <travis@sensoriumembedded.com>
2026-07-18 08:54:58 -0700

    Expansion Port test- placeholder.   TR+ Only

---
+++ Source/C64/ExpansionPortTest/source/StringFunctions.asm
@@ -0,0 +1,332 @@
+; MIT License
+; 
+; Copyright (c) 2023 Travis Smith
+; 
+; Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+; and associated documentation files (the "Software"), to deal in the Software without 
+; restriction, including without limitation the rights to use, copy, modify, merge, publish, 
+; distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom 
+; the Software is furnished to do so, subject to the following conditions:
+; 
+; The above copyright notice and this permission notice shall be included in all copies or 
+; substantial portions of the Software.
+; 
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+; BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+; DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+   EscC              = $01 ;Escape Token for text color ref or spaces, must be otherwise unused
+   EscArgMask        = $c0 ;top 2 bits identify EscCharArg Type
+   EscArgSpaces      = $80 ;indicates number of spaces in lower 6 bits
+   EscArgColorRef    = $00 ;Always 0 (default): indicates color ref # in lower 6 bits
+
+
+PrintSerialString:
+   ;load Acc with RegSerialStringSelect # that will be serialized out from TR port
+   sta rwRegSerialString+IO1Port   ;selects message and resets to start of string
+PrintSerialStringLoaded: ;message already selected
+!ifndef DbgOffline {
+-  lda rwRegSerialString+IO1Port
+   beq +
+   jsr SendChar
+   jmp -
+}
++  rts
+ 
+PrintString:
+   ;prints from C64 RAM location: with Esc modifiers
+   ;   usage: lda #<MsgM2SPolyMenu,  ldy #>MsgM2SPolyMenu,  jsr PrintString 
+   sta smcPrintStringAddr+1
+   sty smcPrintStringAddr+2
+   ldy #0  ;Y will point to current offset throughout
+
+ProcessNextChar  
+   jsr GetNextChar
+   cmp #0
+   beq ++   ;zero terminates string
+   cmp #EscC
+   beq +
+   jsr SendChar ;normal char, send it
+   jmp ProcessNextChar 
+
+   ; process special escape char
++  jsr GetNextChar  ;Esc char argument
+   ; check for special function type
+   tax ;preserve value
+   and #EscArgMask
+   cmp #EscArgSpaces
+   bne +
+   ;print spaces
+   txa
+   and #($ff-EscArgMask)  ;strip arg type
+   tax
+   lda #' '
+-  jsr SendChar
+   dex
+   bne -
+   jmp ProcessNextChar
+   ; assume it's a color reference: 
++  lda TblEscC, x
+   sta $0286  ;set text color
+   jmp ProcessNextChar
+
+GetNextChar  ;load next char in to acc and and increment pointer (Y then smc)
+;y hold offset pointer, preserve throughout PrintString and GetNextChar
+smcPrintStringAddr
+   lda $fffe, y
+   iny
+   bne ++
+   inc smcPrintStringAddr+2
+   ;bne - No roll-over protection
+++ rts   
+
+PrintBanner:
+   lda #<MsgBanner1
+   ldy #>MsgBanner1
+   jsr PrintString 
+   lda #rsstVersionNum
+   jsr PrintSerialString
+   lda #<MsgBanner2
+   ldy #>MsgBanner2
+   jsr PrintString 
+   sec
+   jsr SetCursor ;read column into y reg
+   lda #ChrSpace ;spaces to the end of the line
+-  jsr SendChar  
+   iny
+   cpy #40
+   bne -
+   lda #ChrRvsOff
+   jsr SendChar   
+   rts
+   
+DisplayTime:
+   ldx #1 ;row
+   ldy #29  ;col
+   clc
+   jsr SetCursor
+   lda TblEscC+EscTimeColor
+   sta $0286  ;set text color
+   lda TODHoursBCD ;latches time in regs (stops incrementing)
+   tay ;save for re-use
+
+smc24HourClockDisp   
+   ldx #00 ; x reg holds 24(non-zero) vs 12(zero) hr display 
+   beq Print12Hours
+
+   ;print hours (24 hour format)
+   lda #ChrSpace ;two spaces if 24 hour mode
+   jsr SendChar
+   jsr SendChar
+   tya
+   cmp #$12  ;12am special case
+   bne +     
+   ;print 0
+   lda #$00
+   beq ++ ;always jump
++  cmp #$92  ;12pm special case
+   beq +     ;print as-is (without PM bit)
+   and #$80  ;check am/pm bit
+   beq +     ;print am hour as-is
+   ; pm, but not 12pm, add 12 in decimal mode
+   tya
+   and #$1f
+   clc
+   sei ;disable ints for decimal mode
+   sed ;set decimal mode
+   adc #$12  ;add 12 in BCD
+   cld ;exit decimal mode
+   cli ;ints back on
+   jmp ++
++  tya
+   and #$1f
+++ jsr PrintHexByte
+   jmp Print_mm_ss
+   
+Print12Hours ;print hours (12 hour format)
+   and #$1f
+   bne nz   ;if hours is 0, make it 12...
+   tya
+   ora #$12
+   tay ;re-save for re-use
+nz tya     ;first digit is 1 or blank
+   and #$10
+   bne +
+   lda #ChrSpace
+   jmp ++
++  lda #'1'
+++ jsr SendChar
+   tya
+   and #$0f  ;ones of hours
+   jsr PrintHexNibble
+
+Print_mm_ss   ;print :mm:ss  read 10ths
+   lda #':'
+   jsr SendChar
+   lda TODMinBCD
+   jsr PrintHexByte
+   lda #':'
+   jsr SendChar
+   lda TODSecBCD
+   jsr PrintHexByte
+   ;lda #'.'
+   ;jsr SendChar
+   lda TODTenthSecBCD ;have to read 10ths to release latch
+   ;jsr PrintHexNibble
+   cpx #00 ; x reg holds 24(non-zero) vs 12(zero) hr display 
+   bne +++
+   ;print am/pm if in 12 hour mode
+   tya ;am/pm (pre latch release)
+   and #$80
+   bne +
+   lda #'a'
+   jmp ++
++  lda #'p'
+++ jsr SendChar
+   lda #'m'
+   jsr SendChar
++++rts
+
+PrintIntByte: 
+   ;Print acc as int, no padding
+;Hundreds 
+   ldx #0
+   cmp #10
+   bmi Ones ;skip 100s/10s if <10
+-  cmp #100
+   bmi +     ;loop until below 100
+   sec       ;set to subtract without carry
+   sbc #100   ;subtract 100
+   inx      ;100s place in X
+   jmp -
+
++  cpx #0
+   beq Tens  ;skip 100s if <100
+   tay  ;preserve remainder in Y
+   txa
+   clc
+   adc #'0'
+   jsr SendChar
+   tya
+
+Tens   
+   ldx #0
+-  cmp #10
+   bmi +     ;loop until below 10
+   sec       ;set to subtract without carry
+   sbc #10   ;subtract 10
+   inx
+   jmp -
+
++  tay  ;preserve remainder
+   txa
+   clc
+   adc #'0'
+   jsr SendChar
+   tya
+   
+Ones
+   clc
+   adc #'0'
+   jsr SendChar
+
+   rts
+   
+PrintHexByte:
+   ;Print byte value stored in acc in hex (2 chars)
+   ;trashes acc, X and Y unchanged
+   pha
+   lsr
+   lsr
+   lsr
+   lsr
+   jsr PrintHexNibble
+   pla
+   ;pha   ; preserve acc on return?
+   jsr PrintHexNibble
+   ;pla
+   rts
+   
+PrintHexNibble:   
+   ;Print value stored in lower nible acc in hex
+   ;trashes acc, X and Y unchanged
+   and #$0f
+   cmp #$0a
+   bpl l 
+   clc
+   adc #'0'
+   jmp pr
+l  clc
+   adc #'a'-$0a
+pr jsr SendChar
+   rts
+
+;PrintOnOff:
+;   ;Print "On" or "Off" based on Zero flag
+;   ;uses A and Y regs
+;   bne +
+;   lda #<MsgOff
+;   ldy #>MsgOff
+;   jmp ++
+;+  lda #<MsgOn
+;   ldy #>MsgOn
+;++ jsr PrintString 
+;   rts
+
+;AnyKeyMsgWait:
+;   lda #<MsgAnyKey  ;wait for any key to continue 
+;   ldy #>MsgAnyKey
+;   jsr PrintString 
+;-  jsr GetIn    
+;   beq -
+;   rts
+;
+;PrintFileName:
+;   ; Acc contains rCtlMake*StrWAIT (ie rCtlMakeKernalStrWAIT)
+;   ; X and Y are row, col to print it at
+;   stx smcRow+1
+;   sty smcCol+1
+;   sta wRegControl+IO1Port
+;   jsr WaitForTRWaitMsg   ;moves cursor to upper right
+;smcRow
+;   ldx #7 ;row
+;smcCol
+;   ldy #2 ;col
+;   clc
+;   jsr SetCursor
+;   lda TblEscC+EscMenuMiscColor
+;   sta $0286  ;set text color
+;   lda #ChrQuote
+;   jsr SendChar
+;   jsr PrintSerialStringLoaded
+;   lda #ChrQuote
+;   jsr SendChar
+;   rts
+;
+
+MsgBanner1:  ;set color before clearing for char poke default  
+   !tx EscC,EscNameColor, ChrClear, EscC,EscTRBannerColor, ChrToLower, ChrRvsOn, EscC,EscArgSpaces+9, 0
+MsgBanner2:  
+   !tx " Test", 0
+;MsgAnyKey:
+;   !tx ChrReturn, EscC,EscOptionColor, "Press any key to return"
+;   !tx 0
+;MsgOn:
+;   !tx "On ", 0
+;MsgOff:
+;   !tx "Off", 0
+
+TblEscC:  ;order matches enum ColorRefOffsets
+        ;Main local storage for string escape token (EscC) next character cross-reference
+        ;Local Default     EEPROM default  Description
+        ;EscMenuMiscColor = EscNameColor  in Menu_Regs.i
+   !byte PokeBlack      ;PokeBlack       ; EscBackgndColor     = 0 ; Screen Background
+   !byte PokePurple     ;PokeDrkGrey     ; EscBorderColor      = 1 ; Screen Border
+   !byte PokePurple     ;PokeDrkGrey     ; EscTRBannerColor    = 2 ; Top of screen banner color
+   !byte PokeOrange     ;PokeWhite       ; EscTimeColor        = 3 ; Time Display & Waiting msg
+   !byte PokeYellow     ;PokeLtGrey      ; EscOptionColor      = 4 ; Input key option indication
+   !byte PokeLtBlue     ;PokeDrkGrey     ; EscSourcesColor     = 5 ; General text/descriptions
+   !byte PokeLtGreen    ;PokeMedGrey     ; EscNameColor        = 6 ; FIle names and information
+
--- Source/Teensy/MainMenuItems.h
+++ Source/Teensy/MainMenuItems.h
@@ -54,6 +54,7 @@
 #include "TRMenuFiles/ROMs/TRHelpScreens.prg.h"
 #include "TRMenuFiles/ROMs/TRExtPortCheck.prg.h"
 #ifdef Fab04_REU
+   #include "TRMenuFiles/ROMs/ExpansionPortTest.prg.h"
    #include "TRMenuFiles/ROMs/reutest.prg.h"
    #include "TRMenuFiles/ROMs/reu_checker_v1.0.prg.h"
 #endif
@@ -289,6 +290,9 @@ StructMenuItem dirTeensyROMSpecific[] =
      rtFilePrg  , IOH_TR_BASIC     , (char*)"BASIC TR Command IO access only"  , (uint8_t*)empty_prg                  , sizeof(empty_prg) ,
      rtBin8kLo  , IOH_TeensyROM    , (char*)"(This) TeensyROM Menu Cart"       , (uint8_t*)TeensyROMC64_bin           , sizeof(TeensyROMC64_bin) ,
      rtFilePrg  , IOH_TeensyROM    , (char*)"TeensyROM External Ports Test"    , (uint8_t*)TRExtPortCheck_prg         , sizeof(TRExtPortCheck_prg) ,
+#ifdef Fab04_REU
+     rtFilePrg  , IOH_TeensyROM    , (char*)"C64-TR Expansion Port Test"       , (uint8_t*)ExpansionPortTest_prg      , sizeof(ExpansionPortTest_prg) ,
+#endif
 //could repeat these here...
    //rtFilePrg  , IOH_ASID         , (char*)"TeensyROM ASID Player    +TR ASID", (uint8_t*)ASIDPlayer_prg             , sizeof(ASIDPlayer_prg) ,
    //rtFilePrg  , IOH_TeensyROM    , (char*)"MIDI2SID          +TeensyROM MIDI", (uint8_t*)MIDI2SID_prg               , sizeof(MIDI2SID_prg) ,
