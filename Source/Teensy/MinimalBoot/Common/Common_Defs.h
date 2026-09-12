c59d959483dc5904eec7f3e75b44a83b9a44a77d
SensoriumEmbedded <travis@sensoriumembedded.com>
2026-09-11 21:48:26 -0700

    Drop td debug marker from version; disable Dbg_SerDMA/Dbg_SerTimChg

--- Source/Teensy/MinimalBoot/Common/Common_Defs.h
+++ Source/Teensy/MinimalBoot/Common/Common_Defs.h
@@ -33,7 +33,7 @@
   // #define DbgSignalSenseReset
 
 
-#define TRVersion              "0.8.0.7td"    //*VERSION*
+#define TRVersion              "0.8.0.7"    //*VERSION*
 #ifdef Fab04_Features
    char strVersionNumber[] = "TeensyROM+ v" TRVersion; 
 #else
--- Source/Teensy/TeensyROM.h
+++ Source/Teensy/TeensyROM.h
@@ -40,8 +40,8 @@
   // #define DbgMsgs_M2S   //MIDI2SID MIDI handler messages
 
 //Special use serial commands:
-   #define Dbg_SerDMA    //Serial commands that do direct DMAs.
-   #define Dbg_SerTimChg //Serial commands that tweak timing parameters.
+  // #define Dbg_SerDMA    //Serial commands that do direct DMAs.
+  // #define Dbg_SerTimChg //Serial commands that tweak timing parameters.
   // #define Dbg_SerSwift  //Serial commands that tweak SwiftLink parameters.
   // #define Dbg_SerMem    //Serial commands that display memory info
   // #define Dbg_SerASID   //Serial commands that test the ASID player + queue adjust info
