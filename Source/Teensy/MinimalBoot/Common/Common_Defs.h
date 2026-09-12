af1c856a415730f05c0ab7fe4162770b59844739
SensoriumEmbedded <travis@sensoriumembedded.com>
2026-09-11 20:23:20 -0700

    Bump version to 0.8.0.7td; enable DMA/timing debug serial commands

--- Source/Teensy/MinimalBoot/Common/Common_Defs.h
+++ Source/Teensy/MinimalBoot/Common/Common_Defs.h
@@ -33,7 +33,7 @@
   // #define DbgSignalSenseReset
 
 
-#define TRVersion              "0.8.0.6"    //*VERSION*
+#define TRVersion              "0.8.0.7td"    //*VERSION*
 #ifdef Fab04_Features
    char strVersionNumber[] = "TeensyROM+ v" TRVersion; 
 #else
